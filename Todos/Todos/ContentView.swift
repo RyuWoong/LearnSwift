fe885a1a97ca4999beb74dde403ee0fc2e9e042f
RyuWoong <wjdfbd1121@gmail.com>
2024-09-16 05:07:42 +0900

    faet: 나만의 투두Base 프로젝트 생성

---
+++ Todos/Todos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todos
+//
+//  Created by 박정륭 on 9/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todos/Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by 박정륭 on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
